f71dfd58736f6ece8acf3cce045bc28465bbb22c
Syria LEMOINE <lsyr2006@gmail.com>
2025-05-14 21:56:48 +0200

    Added str_modif

---
+++ str_modif.py
@@ -0,0 +1,27 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def str_modif (file, ajout):
+    #trouver le format (regarder après le dernier point)
+    n = len(file)
+    i = n - 1
+    l_file = list(file)
+    s = [] 
+    while l_file[i] != "." :
+        i= i-1
+    t = i 
+    for t in range (i, n):
+       s.append( l_file[i] )
+       del l_file[i] 
+    
+    l_ajout = list(ajout)
+    
+    l_file.extend(l_ajout)
+    l_file.extend(s)
+    final = "".join(l_file)
+    
+    return final 
+
+
+f = str_modif("Minutiae/empreinte8.jpg", "_pretraitement")   
+print(f)
