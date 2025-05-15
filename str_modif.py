c85399f9c83231c78ed74b214fb5b9dfb21d80e7
Syria <lsyr2006@gmail.com>
2025-05-15 16:22:28 +0200

    str avec changement de dossier opérationnel

--- str_modif.py
+++ str_modif.py
@@ -1,30 +1,58 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def str_modif (file, ajout):
+def str_modif (file, ajout, dossier):
     #trouver le format (regarder après le dernier point)
     n = len(file)
     i = n - 1
+    j = 0
+    t = 0
+    f = 0
+    
+    
     l_file = list(file)
-    s = [] 
-    while (l_file[i] != '\\' and l_file[i] != '/') :
+    s1 = [] 
+    s2=list(dossier)
+    s3=[]
+    s4=[ajout]
+    s5=[]
+    
+    
+    while l_file[i] != "." :
         i= i-1
-    t = i 
-    for t in range (i, n):
-       s.append( l_file[i] )
-       del l_file[i] 
     
-    l_ajout = list(ajout)
     
-    l_file.extend(l_ajout)
-    l_file.extend(s)
-    final = "".join(l_file)
+    
+    for j in range (i, n):
+       s5.append( l_file[j] )
+       
+    t = i
+    while l_file[t] != '\\' and l_file[t] != '/' :
+        t= t-1
+    
+    for j in range (t, i):
+       s3.append( l_file[j] )
+       
+    f = t - 1 
+    while l_file[f] != '\\' and l_file[f] != '/' :
+        f= f-1
+    
+    for j in range (0, f+1):
+       s1.append( l_file[j] )
+    
+
+    s1.extend(s2)
+    s1.extend(s3)
+    s1.extend(s4)
+    s1.extend(s5)
+    
+    final = "".join(s1)
     
     return final 
 
 
-
-f = str_modif("Minutiae/input/empreinte8.jpg", "_pretraitement")   
+  
+f = str_modif("Minutiae/input/empreinte8.jpg", "_pretraitement", "output" )   
 print(f)
 
 
