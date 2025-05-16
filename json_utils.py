1b232097d8316eda5c91d389e4661912112b116b
Elise Perret <elise.perret@outlook.com>
2025-05-16 19:32:22 +0200

    add_personne dans struct

---
+++ json_utils.py
@@ -0,0 +1,50 @@
+import json
+data_file = 'base.json'
+
+
+def ajouter_personne(nom_personne, minutiae_compact):
+    """
+    struct de minutiae_compact :
+    [
+        [ [x,y], "type", [x,y] ],\n
+        [ [x,y], "type", [x,y] ], \n
+        ---
+
+]
+    
+    """
+    with open(data_file, "r", encoding="utf-8") as f:
+        base = json.load(f)
+    
+    # modif format 
+    minutiae_struct = []
+    for item in minutiae_compact:
+        coord, typ, orient = item
+        minutiae_struct.append({
+            "coordonées": coord,
+            "type": typ,
+            "orientation": orient
+        })
+
+    # new enttry
+    nouvelle_entree = {
+        "nom": nom_personne,
+        "minutiae": minutiae_struct
+    }
+
+    # Ajout et sauvegarde
+    base.append(nouvelle_entree)
+    with open(data_file, "w", encoding="utf-8") as f:
+        json.dump(base, f, indent=4, ensure_ascii=False)
+
+    print(f"Entrée ajoutée pour {nom_personne}.")
+
+
+
+minutiae_input = [
+    [[10, 20], "bifurcation", [1.5, 2.5]],
+    [[15, 25], "endings", [2.0, 3.0]],
+
+]
+
+ajouter_personne("base.json", "p5", minutiae_input)
