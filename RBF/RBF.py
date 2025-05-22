5f4e649e52d1235fe808172e2972fd73ca3b627f
Syria <lsyr2006@gmail.com>
2025-05-22 14:21:03 +0200

    Début fonction rbf -> rien de très juste

--- RBF/RBF.py
+++ RBF/RBF.py
@@ -1,35 +1,23 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-'''
-Methode 1 : Operation-intensive 
-1) Solve for field vectors y^(x/y/z)
-2) Update volume mesh points.
-=> O(Ns^3)
 
-Methode 2 : Memory-intensive approach 
-1) The system solution is performed in a preprocessing stage
-2) Definition of a single transfer matrix H (Nv*Ns) -> uv = Hus where H= psi * phi^-1 ∈ R^(Nv×Ns)
-=> infeasible for large meshes
-
-=>  we need to implement it using some data-reduction method
+def fun_phi (r) :
+    return (r)
 
 
-Choice of the points : Greedy full point selection algorithm
+def lambda(x, y):
+    return (y/fun_phi(0))
 
-    procedure
-    init: 
-        xa <- {...}
-        f^exact = u^(x/y/z)
-    main
-        while Nactive < Nred do 
-            alpha_a = phi_a^-1 f_a^exact
-            fêval = psi*alpha_a
-            e = F(f^exact - f^eval)
-            i_worst = arg max e(i) (for i =1 to i=Nx)
-            xa <- x(iworst)
-        end while
-    output
-        return xa
-    end procedure 
-''' 
+def s_create (r) :
+    x = find_control_points () #fonction à faire 
+    y = find_position() #fonction à faire 
+    n = len(x)   
+    acc = 0
+    for i in range(0, n ):
+        lamb = lambda( x[i], y[i])
+        phi = fun_phi(abs(r-x[i]))
+        acc = acc + lamb * phi 
+    return acc 
+
+    
