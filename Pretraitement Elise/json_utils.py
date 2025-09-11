57e1592b5e61d77ad076c34d9f460072475b38d0
Syria <lsyr2006@gmail.com>
2025-09-11 16:10:03 +0200

    Mise a jour

---
+++ Pretraitement Elise/json_utils.py
@@ -0,0 +1,58 @@
+import json
+data_file = 'base.json'
+
+
+
+
+def ajouter_personne(nom_personne, minutiae_tab):
+    """
+    struct de minutiae_tab :
+    [
+        [ [x,y], "type", [x,y] ],\n
+        [ [x,y], "type", [x,y] ], \n
+        ---
+
+]
+    
+    """
+
+
+    with open(data_file, "r", encoding="utf-8") as f:
+        base = json.load(f) #ouvre base
+    
+    # modif format pour coller au json
+    minutiae_struct = []
+    for elt in minutiae_tab:
+        coord, typ, orient = elt
+        minutiae_struct.append({
+            "coordonnees": coord,
+            "type": typ,
+            "orientation": orient
+        })
+
+    # new enttry
+    nouvelle_entree = {
+        "nom": nom_personne,
+        "minutiae": minutiae_struct
+    }
+
+    # add+ sauvegarde
+    base.append(nouvelle_entree) #(add)
+
+    with open(data_file, "w", encoding="utf-8") as f:
+        json.dump(base, f, indent=4, ensure_ascii=False) #dump = l'ajoute et sauvegarde dans le fichierc ?
+
+    print(f"add {nom_personne}") #check
+
+
+"""
+test
+ input = [
+    [[10, 20], "bifurcation", [1.5, 2.5]],
+    [[15, 25], "endings", [2.0, 3.0]],
+
+]
+
+ajouter_personne("p6", input)
+
+"""
---
+++ Pretraitement Elise/main_find.py
@@ -0,0 +1,26 @@
+
+from pretraitement import pretraitements
+from find_minutiae import find_minuatiae
+import sys
+import os
+
+# methode pour acceder a utile ??
+parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.append(parent_dir)
+
+from json_utils import ajouter_personne
+
+filename = 'minutiae_detection\\input\\empreinteS3.jpg'
+output_filename = 'minutiae_detection\\output\\empreinteS3_minutiae.jpg' #[ ] a obtenir automatiquement avec str_modify a l'interieur de find_minutiae
+
+
+pretraitée_file_name = pretraitements(filename)
+tab = find_minuatiae(pretraitée_file_name,output_filename) 
+ #[]faire en sorte que ce soit [ [endings [(x,y),orientation] ] , [ biffurcation [(x,y),orientation] ]  ]
+
+print(tab)
+
+ajouter_personne("personne_test",tab)
+
+
+
