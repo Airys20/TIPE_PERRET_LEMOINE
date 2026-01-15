c8314659812d3df98a41ce5da699b186e1c97b66
Elise Perret <elise.perret@outlook.com>
2026-01-15 12:22:15 +0100

    supp dosier inutiles

--- minutiae_detection/test_get_orientation.py
+++
@@ -1,7 +0,0 @@
-import numpy    as np
-X = np.load('minutiae_detection\\output_orientation\\O_bloque.npy', mmap_mode='r')
-for i in X:
-   
-        for j in i:
-            if j>0:
-                print (j)
