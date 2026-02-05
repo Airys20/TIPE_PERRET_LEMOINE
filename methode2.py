6d515ac14ca5c35767e9bd24281e0a497a24348e
Syria <lsyr2006@gmail.com>
2026-02-05 15:54:38 +0100

    rajout des tests de type :)

--- methode2.py
+++ methode2.py
@@ -18,17 +18,17 @@ ANGLE_TOL = 0.05
 _neighbors_cache = {}
 
 def chgt_base(M, t):
-    xm, ym, theta_m = M[t]
+    xm, ym, theta_m, _ = M[t]
     M_new = []
     for i in range(len(M)):
-        xi, yi, theta_i = M[i]
+        xi, yi, theta_i, typ_i = M[i]
         xi -= xm
         yi -= ym
         xtmp = xi
         xi = np.cos(theta_m)*xi + np.sin(theta_m)*yi
         yi = -np.sin(theta_m)*xtmp + np.cos(theta_m)*yi
         theta_new = wrap_angle(theta_i - theta_m)
-        M_new.append((xi, yi, theta_new))
+        M_new.append((xi, yi, theta_new, typ_i))
     return M_new
 
 def fusion(L1, L2):
@@ -60,7 +60,8 @@ def distances(M):
 
 def neighbors(M, nb_nghbr, i):
     key=(id(M),i)
-    if key in _neighbors_cache: return _neighbors_cache[key]
+    if key in _neighbors_cache: 
+        return _neighbors_cache[key]
     M_tmp=chgt_base(M,i)
     M_new=distances(M_tmp)
     n0=[]; j=1
@@ -88,10 +89,15 @@ def apply_transformation(minutiae,R,c,t):
     new_minutiae=[]
     angle_rot=np.arctan2(R[1,0],R[0,0])
     for m in minutiae:
-        x,y=m["coordonnees"]; theta=m["orientation"]
+        x,y=m["coordonnees"]
+        theta=m["orientation"]
         xy_new=t + c*(R@np.array([x,y]))
         theta_new=wrap_angle(theta + angle_rot)
-        new_minutiae.append({"coordonnees":xy_new.tolist(),"orientation":theta_new})
+        new_minutiae.append({
+            "coordonnees": xy_new.tolist(),
+            "orientation": theta_new,
+            "type": m["type"]
+        })
     return new_minutiae
 
 ''' *************************************** MATCHING *************************************** '''
@@ -102,10 +108,14 @@ def classify(minutiae_recherche,minutiae_catalogue):
     for i in range(nc):
         xc,yc=minutiae_catalogue[i]["coordonnees"]
         dc=minutiae_catalogue[i]["orientation"]
+        tc=minutiae_catalogue[i]["type"]
         best=0
         for m in minutiae_recherche:
             xr,yr=m["coordonnees"]
             dr=m["orientation"]
+            tr=m["type"]
+            if tr != tc:
+                continue
             if abs(xr-xc)<SPATIAL_TOL and abs(yr-yc)<SPATIAL_TOL:
                 if angle_diff_biom(dr,dc)<ANGLE_TOL:
                     best=2; break
@@ -120,25 +130,35 @@ def matching_score(minutiae_catalogue,paired):
 
 ''' *************************************** MATCHING GLOBAL *************************************** '''
 
-MIN_REF = 15
+MIN_REF = 30
 NB_NGHBR = 15
-LIM_THETA = 0.5
+LIM_THETA = 0.3
 
 def global_matching_reference(data_catalogue,data_recherche):
 
-    with open(data_catalogue,"r") as f: catalogue=json.load(f)
-    with open(data_recherche,"r") as f: recherche=json.load(f)
+    with open(data_catalogue,"r") as f: 
+        catalogue=json.load(f)
+    with open(data_recherche,"r") as f: 
+        recherche=json.load(f)
 
-    M_recherche=[(m["coordonnees"][0],m["coordonnees"][1],m["orientation"])
-                 for m in recherche[0]["minutiae"]]
+    M_recherche=[(
+        m["coordonnees"][0],
+        m["coordonnees"][1],
+        m["orientation"],
+        m["type"]
+    ) for m in recherche[0]["minutiae"]]
 
     best_score=0; best_name=None
 
     for p in range(len(catalogue)):
         print("Analyse de :", catalogue[p]["nom"])
 
-        M_catalogue=[(m["coordonnees"][0],m["coordonnees"][1],m["orientation"])
-                     for m in catalogue[p]["minutiae"]]
+        M_catalogue=[(
+            m["coordonnees"][0],
+            m["coordonnees"][1],
+            m["orientation"],
+            m["type"]
+        ) for m in catalogue[p]["minutiae"]]
 
         ref_r=[]; ref_c=[]
         used_r=set(); used_c=set()
@@ -146,7 +166,12 @@ def global_matching_reference(data_catalogue,data_recherche):
         for i in range(len(M_recherche)):
             for j in range(len(M_catalogue)):
 
-                if abs(wrap_angle(M_recherche[i][2]-M_catalogue[j][2]))>LIM_THETA:
+                # orientation compatible
+                if abs(wrap_angle(M_recherche[i][2]-M_catalogue[j][2])) > LIM_THETA:
+                    continue
+
+                # même type de minutie (ending / bifurcation)
+                if M_recherche[i][3] != M_catalogue[j][3]:
                     continue
 
                 ngh_r = neighbors(M_recherche, NB_NGHBR, i)
@@ -159,7 +184,8 @@ def global_matching_reference(data_catalogue,data_recherche):
                 if i not in used_r and j not in used_c:
                     ref_r.append(M_recherche[i][:2])
                     ref_c.append(M_catalogue[j][:2])
-                    used_r.add(i); used_c.add(j)
+                    used_r.add(i)
+                    used_c.add(j)
 
         if len(ref_r) < MIN_REF:
             print("Références insuffisantes :", len(ref_r))
--- minutiae_detection/main_find.py
+++ minutiae_detection/main_find.py
@@ -15,9 +15,9 @@ from json_utils import ajouter_personne
 
 
 #REGLAGES 
-NOM = 'Syria2'
+NOM = 'Elise1'
 BASE = "recherche.json"
-FILENAME='minutiae_detection\input\\Syria2.jpeg' #lien ou tu mets l'img a tester
+FILENAME='minutiae_detection\input\\Elise1.jpeg' #lien ou tu mets l'img a tester
 output_filename = 'minutiae_detection/passage_main/' + NOM + '_output_minutiae.jpg' #lien ou tu stocke l'img superposée des minuties
 
 
